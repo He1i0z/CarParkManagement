65e0a40a674a806b1ef8f0666e42cf71db9b41b4
noternestt <2305050c@student.tp.edu.sg>
2024-12-08 18:15:26 +0800

    Revert "scaffolded all the pages"
    This reverts commit b5346658994d4269c58bfb79309e354f58da23e3.

--- CarParkManagement/Program.cs
+++ CarParkManagement/Program.cs
@@ -1,15 +1,6 @@
-﻿using CarParkManagement.Components;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using CarParkManagement.Data;
+using CarParkManagement.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContextFactory<CarParkManagementContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CarParkManagementContext") ?? throw new InvalidOperationException("Connection string 'CarParkManagementContext' not found.")));
-
-builder.Services.AddQuickGridEntityFrameworkAdapter();
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -23,7 +14,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
